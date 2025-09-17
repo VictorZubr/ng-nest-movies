15a98b4ade298037aa022fe9e22f1de81c6b165c
Victor Zubrinkin <zubr@rambler.ru>
2025-09-17 19:02:28 +0300

    feat: add roxy config

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs';
 
 import { Movie } from '../types/types';
 
+const API_PREFIX = '/api';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,22 +15,22 @@ export class MoviesService {
 
   getMovies(searchName: string): Observable<Movie[]> {
     const params = new HttpParams().set('search', searchName);
-    return this.http.get<Movie[]>('/movies', { params });
+    return this.http.get<Movie[]>(`${API_PREFIX}/movies`, { params });
   }
 
   addMovie(name: string): Observable<Movie> {
-    return this.http.post<Movie>('/movies', { name });
+    return this.http.post<Movie>(`${API_PREFIX}/movies`, { name });
   }
 
   updateMovie(movie: Movie): Observable<Movie> {
-    return this.http.put<Movie>(`/movies/${movie.id}`, movie);
+    return this.http.put<Movie>(`${API_PREFIX}/movies/${movie.id}`, movie);
   }
 
   deleteMovie(id: number): Observable<Movie> {
-    return this.http.delete<Movie>(`/movies/${id}`);
+    return this.http.delete<Movie>(`${API_PREFIX}/movies/${id}`);
   }
 
   getMovieById(id: number): Observable<Movie> {
-    return this.http.get<Movie>(`/movies/${id}`);
+    return this.http.get<Movie>(`${API_PREFIX}/movies/${id}`);
   }
 }
